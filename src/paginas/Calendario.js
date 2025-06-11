a6592c264f27e6ec97239e317d199f587b205ac4
Art-26 <arthurortbach@gmail.com>
2025-06-11 19:16:15 -0300

    Atualização nos filmes que teremos futuramente no ano de 2025

--- src/paginas/Calendario.js
+++ src/paginas/Calendario.js
@@ -50,23 +50,23 @@ const dadosFuturos = [
     {
         id: 1,
         titulo: 'Lilo & Stitch',
-        dataLancamento: '2000-10-20',
-        descricao: 'Filme Lilo & Stitch',
+        dataLancamento: '22 de maio de 2025',
+        descricao: 'Live-action do famoso clássico de animação da Disney, Lilo & Stitch conta a história da amizade entre uma jovem menina humana e um alienígena fugitivo que parece um cachorro. Stitch (Chris Sanders), o experimento 626, é um extraterrestre expressivo que é adotado como animal de estimação por Lilo (Maia Kealoha), uma imaginativa e rebelde garota havaiana, e juntos eles descobrem o significado de família. O jeito extrovertido de Lilo mais do que corresponde à energia caótica do pequeno monstro peludo e impulsivo que está sendo caçado pelos agentes da Federação Galáctica Unida. A amizade incomum entre os dois provoca uma série de confusões e problemas até com a assistente social que cuida de Lilo, observando seu bem-estar ao lado da irmã Nani, sua tutora legal desde a morte dos pais.',
         poster: 'https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/cd5df8d5-ab52-11ef-aa79-9bebc91072b3/lilo-stitch-live-action+(1).jpeg',
     },
     {
         id: 2,
-        titulo: 'Lilo & Stitch',
-        dataLancamento: '2000-10-20',
-        descricao: 'Filme Lilo & Stitch',
-        poster: 'https://d2d7ho1ae66ldi.cloudfront.net/',
+        titulo: 'SUPERMAN',
+        dataLancamento: '10 de julho de 2025',
+        descricao: 'Um herói movido pela crença e pela esperança na bondade da humanidade. Em Superman, acompanhamos a jornada do super-herói em tentar conciliar suas duas personas: sua herança extraterrestre como kryptoniano e sua vida humana, criado como Clark Kent (David Corenswet) na cidade de Smallville no Kansas. Dirigido por James Gunn, o novo filme irá reunir personagens, heróis e vilões clássicos da história de Superman, como Lex Luthor (Nicholas Hoult), Lois Lane (Rachel Brosnahan), Lanterna Verde (Nathan Fillion), Mulher-Gavião (Isabela Merced), entre outros. O chamado de Superman será colocado à prova através de uma série de novas aventuras épicas e diante de uma sociedade que enxerga seus valores de justiça e verdade como antiquados.',
+        poster: 'https://saocarlosnotoque.com/wp-content/uploads/2025/03/superman-filme-estreia-2025.jpg',
     },
     {
         id: 3,
-        titulo: 'Lilo & Stitch',
-        dataLancamento: '2000-10-20',
-        descricao: 'Filme Lilo & Stitch',
-        poster: 'https://d2d7ho1ae66ldi.cloudfront.net/',
+        titulo: 'Zootopia',
+        dataLancamento: '27 de novembro de 2025',
+        descricao: 'Os heróis e policiais novatos Judy Hopps e Nick Wilde estão de volta para mais uma aventura extravagante pela grande metrópole animal. Em Zootopia 2, Judy e Nick precisarão formar uma parceria inusitada com um recém-chegado à cidade: o misterioso réptil Gary De’Snake. Para tentar desvendar os mistérios do caso envolvendo a víbora, Judy e Nick devem desvendar novas partes da cidade, sendo testados o tempo todo.',
+        poster: 'https://i.ytimg.com/vi/KYOguImkYek/maxresdefault.jpg',
     },
 ];
 
