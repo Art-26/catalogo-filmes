748b86e2c39b8a6fbe5f250a43e4fdb0fcd9a85c
Art-26 <arthurortbach@gmail.com>
2025-06-11 20:57:08 -0300

    Edição no arquivo de Logo.js, para uma melhor leitura do logotipo do projeto

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 const LogoContainer = styled.div`
   font-family: 'Exile', cursive;
   font-size: 4rem;
-  font-weight: bold;
+  font-weight: light;
   text-align: center;
   margin: 1rem 0;
-  background: linear-gradient(90deg, #bfff00, #99cc00);
+  background: linear-gradient(40deg, #bfff00, #99cc00);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   filter: drop-shadow(0 0 2px #bfff00) drop-shadow(0 0 5px #99cc00);
